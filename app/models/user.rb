4c676305487393ed2a01b9760e78ed71fc530318
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-13 01:28:01 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX }
-  validates :nickname, presence: true, presence: { message: " can't be blank"}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: " can't be black"}
-  validates :first_name, presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: " can't be blank"}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ ,message: " can't be blank"}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ ,message: " can't be blank"}
-  validates :birth_date, presence: true, presence: {message: " can't be blank"} 
-
+  validates :nickname, presence: true, presence: { message: " can't be blank" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " can't be black" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: " can't be blank" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: " can't be blank" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: " can't be blank" }
+  validates :birth_date, presence: true, presence: { message: " can't be blank" }
 end
