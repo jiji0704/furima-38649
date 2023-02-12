a02822f4fe8cf8c651933d997140ee81008bfb72
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-12 23:48:43 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password, format: { with: PASSWORD_REGEX }
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－龥々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－龥々ー]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true 
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :nickname, presence: true, presence: { message: 'Nickname cant be black'}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: 'Lirst name cant be black'}
+  validates :first_name, presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: 'Fast name cant be black'}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ ,message: 'Last name kana cant be black'}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ ,message: 'first name kana cant be black'}
+  validates :birthday, presence: true, presence: {message: 'Birth date cant be black'} 
 
 end
