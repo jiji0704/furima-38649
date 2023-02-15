748cc3bae6deac5fcd772c626de329b16b3db128
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-15 22:02:56 +0900

    テストコードを「正常系」と「異常系』に分ける

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+   context '新規登録できるとき' do
     it 'nicknameとemail、passwordとpassword_confirmationと氏名と生年月日が存在すれば登録できる' do
       expect(@user).to be_valid
     end
+   end
+
+   context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -96,5 +100,6 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Birth date can't be blank")
     end
+   end
   end
 end
