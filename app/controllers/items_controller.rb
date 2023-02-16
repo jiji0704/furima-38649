13e8cf5b890603b145f52fc30a9eb753f364b00e
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-16 23:22:34 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  #画像の保存を許可するストロングパラメーターにしましょう
+  # private
+
+  # def item_params
+  #   params.require(:item).permit(:image).merge(user_id: current_user.id)
+  # end
 end
