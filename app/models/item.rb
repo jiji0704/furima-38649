3634ebc8ea8b9c51a3a40a6e90e1f79b6e97cffc
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-16 23:31:02 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name , presence: true
+  validates :item_information , presence: true
+  validates :item_category_id , presence: true
+  validates :item_sales_status_id , presence: true
+  validates :item_shipping_fee_status_id , presence: true
+  validates :prefecture_id , presence: true
+  validates :item_scheduled_delivery_id , presence: true
+  validates :item_price , presence: true
+  validates :image, presence: true
+
 end
