513630aa7c6458a79aa33ce66bb967e30f906f99
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-18 19:28:38 +0900

    ログイン・エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :schedued_delivery, optional: true
   has_one_attached :image
 
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :item_information, presence: true
   validates :item_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
@@ -15,6 +16,7 @@ class Item < ApplicationRecord
   validates :item_shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_scheduled_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
-  validates :image, presence: true
+  validates :item_price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, allow_blank: true, message: 'is invalid.Input half-width characters.'}
+  validates :item_price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: 'is out of setting range'}
+
 end
