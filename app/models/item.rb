69924ca5df350b8ec11486bdc1a8b4ec384479a5
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-18 14:50:44 +0900

    販売手数料の計算

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,6 @@ class Item < ApplicationRecord
   validates :item_shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_scheduled_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_price, presence: true
+  validates :item_price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
   validates :image, presence: true
 end
