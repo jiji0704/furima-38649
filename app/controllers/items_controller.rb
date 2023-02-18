91483d9867a810591602315303e9683aa15c18de
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-18 13:25:15 +0900

    カテゴリーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,16 +3,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect to root_path
+    else
+      render new
   end
 
 
-  #画像の保存を許可するストロングパラメーターにしましょう
-  # private
+  private
 
-  # def item_params
-  #   params.require(:item).permit(:image).merge(user_id: current_user.id)
-  # end
+  def item_params
+    params.require(:item).permit(:item_name, :item_information, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :item_price, :image ).merge(user_id: current_user.id)
+  end
+
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :catedory
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :schedued_delivery
   has_one_attached :image
 
-  validates :item_name , presence: true
-  validates :item_information , presence: true
-  validates :item_category_id , presence: true
-  validates :item_sales_status_id , presence: true
-  validates :item_shipping_fee_status_id , presence: true
-  validates :prefecture_id , presence: true
-  validates :item_scheduled_delivery_id , presence: true
-  validates :item_price , presence: true
+  validates :item_name, presence: true
+  validates :item_information, presence: true
+  validates :item_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_sales_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_scheduled_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_price, presence: true
   validates :image, presence: true
-
 end
