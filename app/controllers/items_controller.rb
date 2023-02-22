c727615fb0faf7eb5eab05cff49a9790569b5607
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-22 23:11:18 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :move_to_index, except:[:index, :show]
-  before_action :ensure_user, only:[:edit, :update]
+  before_action :move_to_index, except: [:index, :show]
+  before_action :ensure_user, only: [:edit, :update]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -46,16 +46,15 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to root_path
-    end
+    return if user_signed_in?
+
+    redirect_to root_path
   end
 
   def ensure_user
-    @item= Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
-  end
+    @item = Item.find(params[:id])
+    return if @item.user_id == current_user.id
 
+    redirect_to root_path
+  end
 end
