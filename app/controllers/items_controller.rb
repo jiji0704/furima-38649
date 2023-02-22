6d6873a3b2cf9cfa85be31bf7f89fcd3cdf74761
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-02-22 23:08:18 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, except:[:index, :show]
+  before_action :ensure_user, only:[:edit, :update]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -23,9 +25,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update_attributes(item_params)
+    if @item.save
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
@@ -34,4 +44,18 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id,
                                  :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  def ensure_user
+    @item= Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
 end
