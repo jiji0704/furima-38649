6a2f46cde026bb51bba42148834f2504bef5ab39
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-03-07 00:46:33 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,15 +13,15 @@ class Item < ApplicationRecord
     validates :images
     validates :name
     validates :info
-    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :price, numericality: { only_integer: true, message: 'is invalid.Input half-width characters.' }
+    validates :category_id, numericality: { other_than: 1, message: "を入力してください" }
+    validates :sales_status_id, numericality: { other_than: 1, message: "を入力してください" }
+    validates :shipping_fee_status_id, numericality: { other_than: 1, message: "を入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
+    validates :scheduled_delivery_id, numericality: { other_than: 1, message: "を入力してください" }
+    validates :price, numericality: { only_integer: true, message: 'は無効です。半角数字で入力してください' }
   end
   validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   validates :price,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                            message: 'is out of setting range' }
+                            message: 'は設定範囲外です' }
 end
