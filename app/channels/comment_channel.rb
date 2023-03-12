13bc7037fdeae384cff20deae8b52cccc7c9991a
jiji0704 <chocojiji0704@yahoo.co.jp>
2023-03-12 14:56:07 +0900

    即時反映機能

---
+++ app/channels/comment_channel.rb
@@ -0,0 +1,12 @@
+class CommentChannel < ApplicationCable::Channel
+  def subscribed
+    @item = Item.find(params[:item_id]) # 追記
+    stream_for @item # 追記
+    
+    # stream_from "comment_channel" #追加
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @item = Item.find(params[:item_id]) #追加
     if @comment.save
-      redirect_to item_path(params[:item_id])
+      CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user } #追加
+      # ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user} #追加
+      # redirect_to item_path(params[:item_id])
     end
   end
 
